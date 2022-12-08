df4c17d7d0d9961b3e710760ff9489e69cc94088
Zink321 <82705904+Zink321@users.noreply.github.com>
2022-12-08 20:43:17 +0700

    Update email_spammer.py

--- email_spammer.py
+++ email_spammer.py
@@ -2,70 +2,47 @@ import smtplib as root
 from email.mime.text import MIMEText
 from email.header import Header
 from email.mime.multipart import MIMEMultipart
+import art
 
 
-
-#меню (бета)
+#watermark
 def menu():
-	print("░█▀▀▀ ░█▀▄▀█ ─█▀▀█ ▀█▀ ░█─── 　 ░█▀▀▀█ ░█▀▀█ ─█▀▀█ ░█▀▄▀█ ░█▀▄▀█ ░█▀▀▀ ░█▀▀█ ")
-	print("░█▀▀▀ ░█░█░█ ░█▄▄█ ░█─ ░█─── 　 ─▀▀▀▄▄ ░█▄▄█ ░█▄▄█ ░█░█░█ ░█░█░█ ░█▀▀▀ ░█▄▄▀ ")
-	print("░█▄▄▄ ░█──░█ ░█─░█ ▄█▄ ░█▄▄█ 　 ░█▄▄▄█ ░█─── ░█─░█ ░█──░█ ░█──░█ ░█▄▄▄ ░█─░█ ")
-	print('                                                                      By Zink')
-	print('')
+	art.tprint("Email Spammer")
+	print("\nFor Mail.ru :)")
 menu()
 
-
-#smtp выбор
-def smtpchoose():
-	print('Choose smtp server (1-3)')
-	print('\n1. smtp.gmail.com (didn`t work) \n2. smtp.yandex.ru \n3. smtp.mail.ru')
-	a = int(input('Your choose: '))
-
-	match a:
-		case 1:
-			url = 'smtp.gmail.com'
-		case 2:
-			url = 'smtp.yandex.ru'
-		case 3:
-			url = 'smtp.mail.ru'
-
-	if (a > 3 or a < 1):
-		print('Invalid choose \n')
-
-	return url
-
-#получаем ссылку smtp сервера
-url = smtpchoose()
-
-
-
-#блок данных для входа и сообщения
+#user data block (messages, recipient and etc)
 def user_data():
 	login = input('\nLogin: ')
 	password = input('\nPassword: ')
 	komu = input('\nTo: ')
 	zagolovok = input('\nTopic: ')
 	message = input('\nMessage: ')
+	counter = input('\nHow many you want: ')
 
-	return login, password, komu, zagolovok, message
+	return login, password, komu, zagolovok, message, counter
 
-#получаем данные пользователя
-login, password, komu, zagolovok, message = user_data()
+#gettig user data from return statement
+login, password, komu, zagolovok, message, counter = user_data()
 
-
-
-#функция хуюнкция для отправки
+#function for mail sending with smtp server
 def send_mail():
 
 	msg = MIMEMultipart()
 
 	msg[ 'Subject' ] = zagolovok
 	msg[ 'From' ] = login
-	body = message
-	msg.attach( MIMEText(body, 'plain'))
 
+	msg.attach( MIMEText(message, 'plain'))
 
-	server = root.SMTP_SSL(url, 465)
+	server = root.SMTP_SSL("smtp.mail.ru", 465)
 	server.login(login, password)
 	server.sendmail(login, komu, msg.as_string() )
-send_mail()
+
+#spamming
+def flood():
+	i=0
+	while i < int(counter):
+		send_mail()
+		i+=1
+flood()
